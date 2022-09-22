df2b0fbdee55878688eaa4afd8c26f43ebacf32f
Muhammad Irfan Lutfi <iluthfi469@gmail.com>
2022-09-22 10:43:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intent/TentangActivity.java
@@ -0,0 +1,25 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TentangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+
+        Button kembali = findViewById(R.id.kembali);
+
+        kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
